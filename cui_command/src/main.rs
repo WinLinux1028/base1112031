2ca5c2268a1aeb3c23dd8df0e346c1200135daa8
SovietReimu1228 <reimussr@protonmail.ch>
2023-03-10 06:11:32 +0900

    add cui command

---
+++ cui_command/src/main.rs
@@ -0,0 +1,42 @@
+use std::{io::stdin, str::FromStr};
+
+use base1112031::{FromBase1112031, ToBase1112031};
+use num_bigint::BigUint;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        help();
+        return;
+    }
+    match args[1].as_str() {
+        "--to-base1112031" => to_base1112031(),
+        "--to-base10" => to_base10(),
+        _ => help(),
+    }
+}
+
+fn to_base1112031() {
+    let mut buf = String::new();
+    while stdin().read_line(&mut buf).is_ok() {
+        let input = BigUint::from_str(&buf).unwrap();
+        println!("{}", input.to_base1112031::<String>().unwrap());
+    }
+}
+
+fn to_base10() {
+    let mut buf = String::new();
+    while stdin().read_line(&mut buf).is_ok() {
+        let result: BigUint = FromBase1112031::from_base1112031(buf.as_str()).unwrap();
+        println!("{}", result);
+    }
+}
+
+fn help() {
+    println!("base1112031 generator");
+    println!("Command line options");
+    println!(
+        "--to-base1112031 : Receives a decimal number from stdin and converts it to base1112031."
+    );
+    println!("--to-base10 : Receives a base1112031 from stdin and converts it to decimal number.");
+}
